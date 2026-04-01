75ed389f6196f1dad994a176da1f5d72147c4926
winson junior <58245755+WinsonJunior@users.noreply.github.com>
2026-04-02 03:14:19 +0800

    Delete hooks/block-dangerous.bat

--- hooks/block-dangerous.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo %CLAUDE_TOOL_INPUT_COMMAND% | findstr /i "rm -rf DROP TABLE git push git commit docker vercel railway" >nul
-if %errorlevel%==0 (
-    echo Blocked: dangerous command detected. 1>&2
-    exit 2
-)
-exit 0
